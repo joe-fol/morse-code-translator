d84dbd13f56e60a3932adc0e4ae13cd1579c9888
joe-fol <joefolkard@protonmail.com>
2021-05-14 15:04:07 +0100

    Morse translates

--- dist/main.dev.js
+++ dist/main.dev.js
@@ -1,80 +1,84 @@
 "use strict";
 
-var input = document.querySelector("#morse__input");
-var translateButton = document.querySelector("#morse__translator");
-var morseDisplay = document.querySelector("#morse__window");
-var userInput = []; // holder for user input
-// array of objects, ther key is the leter and value is the morse
-// check the user input against the key and return value to new string
-// - have object with letters/numbers and morse equivalents {a: ".-", b: "-..."}
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
+var input = document.querySelector('#morse__input');
+var translateButton = document.querySelector('#morse__translator');
+var morseDisplay = document.querySelector('#morse__window');
+var reverse = {};
+var userInput = [];
 var alphabet = {
-  "a": ".- ",
-  "b": "-... ",
-  "c": "-.-. ",
-  "d": "-.. ",
-  "e": ". ",
-  "f": "..-. ",
-  "g": "--. ",
-  "h": ".... ",
-  "i": ".. ",
-  "j": ".--- ",
-  "k": "-.- ",
-  "l": ".-.. ",
-  "m": "-- ",
-  "n": "-. ",
-  "o": "--- ",
-  "p": ".--. ",
-  "q": "--.- ",
-  "r": ".-. ",
-  "s": "... ",
-  "t": "- ",
-  "u": "..- ",
-  "v": "...- ",
-  "w": ".-- ",
-  "x": "-..- ",
-  "y": "-.-- ",
-  "z": "--.. "
+  a: '.-',
+  b: '-...',
+  c: '-.-.',
+  d: '-..',
+  e: '.',
+  f: '..-.',
+  g: '--.',
+  h: '....',
+  i: '..',
+  j: '.---',
+  k: '-.-',
+  l: '.-..',
+  m: '--',
+  n: '-.',
+  o: '---',
+  p: '.--.',
+  q: '--.-',
+  r: '.-.',
+  s: '...',
+  t: '-',
+  u: '..-',
+  v: '...-',
+  w: '.--',
+  x: '-..-',
+  y: '-.--',
+  z: '--..'
 };
+
+var invert = function invert(alphabet) {
+  var morseAlphabet = {};
+
+  for (var keyName in alphabet) {
+    if (alphabet.hasOwnProperty(keyName)) {
+      morseAlphabet[alphabet[keyName]] = keyName;
+    }
+  }
+
+  return morseAlphabet;
+};
+
+reverse = invert(alphabet);
+console.log(reverse);
 
 var generateMorse = function generateMorse(alphabet) {
   var morseChecker = userInput.map(function (letter) {
     var morseTranslate = alphabet[letter];
     return morseTranslate;
   });
-  return morseChecker.join("");
-}; // event listner for the translate button when hit will spit back morse or english 
-
-
-translateButton.addEventListener("click", function (event) {
-  event.preventDefault();
-  userInput = input.value.split("");
-  morseDisplay.innerHTML = generateMorse(alphabet);
-});
-var clicked = false;
-
-var toggleButton = function toggleButton() {
-  if (!clicked) {
-    clicked = true;
-    document.querySelector("#morse__selector").innerHTML = "english to morse code";
-  } else {
-    clicked = false;
-    document.querySelector("#morse__selector").innerHTML = "morse to english";
-  }
+  return morseChecker.join('');
 };
 
-var morseToEnglish = function morseToEnglish(alphabet) {
+var generateEnglish = function generateEnglish(reverse) {
+  console.log('inside', reverse);
   var englishChecker = userInput.map(function (morse) {
-    var englishTranslate = alphabet[letter];
+    var englishTranslate = reverse[morse];
     return englishTranslate;
   });
+  console.log(englishChecker);
   return englishChecker.join('');
 };
 
-function getKeyByValue(object, value) {
-  return Object.keys(object).find(function (key) {
-    return object[key] === value;
-  });
-}
+translateButton.addEventListener('click', function (event) {
+  event.preventDefault();
 
-console.log(getKeyByValue(alphabet, ".--"));
+  if (input.value.match(/[a-z0-9]/gi)) {
+    userInput = input.value.toLowerCase().split('');
+    console.log('english', userInput, _typeof(userInput));
+    morseDisplay.innerHTML = generateMorse(alphabet);
+  } else {
+    userInput = input.value.split('/');
+    console.log('morse', userInput, _typeof(userInput));
+    morseDisplay.innerHTML = generateEnglish(reverse);
+  }
+});
--- main.js
+++ main.js
@@ -1,89 +1,83 @@
-const input = document.querySelector("#morse__input");
-const translateButton = document.querySelector("#morse__translator");
-const morseDisplay = document.querySelector("#morse__window");
+const input = document.querySelector('#morse__input');
+const translateButton = document.querySelector('#morse__translator');
+const morseDisplay = document.querySelector('#morse__window');
 
+let reverse = {}
 let userInput = [];
-// holder for user input
-// array of objects, ther key is the leter and value is the morse
-// check the user input against the key and return value to new string
-// - have object with letters/numbers and morse equivalents {a: ".-", b: "-..."}
+
 
 const alphabet = {
-    "a": ".- ",
-    "b": "-... ",
-    "c": "-.-. ",
-    "d": "-.. ",
-    "e": ". ",
-    "f": "..-. ",
-    "g": "--. ",
-    "h": ".... ",
-    "i": ".. ",
-    "j": ".--- ",
-    "k": "-.- ",
-    "l": ".-.. ",
-    "m": "-- ",
-    "n": "-. ",
-    "o": "--- ",
-    "p": ".--. ",
-    "q": "--.- ",
-    "r": ".-. ",
-    "s": "... ",
-    "t": "- ",
-    "u": "..- ",
-    "v": "...- ",
-    "w": ".-- ",
-    "x": "-..- ",
-    "y": "-.-- ",
-    "z": "--.. " 
-  };
-  
+  a: '.-',
+  b: '-...',
+  c: '-.-.',
+  d: '-..',
+  e: '.',
+  f: '..-.',
+  g: '--.',
+  h: '....',
+  i: '..',
+  j: '.---',
+  k: '-.-',
+  l: '.-..',
+  m: '--',
+  n: '-.',
+  o: '---',
+  p: '.--.',
+  q: '--.-',
+  r: '.-.',
+  s: '...',
+  t: '-',
+  u: '..-',
+  v: '...-',
+  w: '.--',
+  x: '-..-',
+  y: '-.--',
+  z: '--..',
+};
+
+const invert = (alphabet) => {
+  const morseAlphabet = {};
+
+  for (const keyName in alphabet) {
+    if (alphabet.hasOwnProperty(keyName)) {
+      morseAlphabet[alphabet[keyName]] = keyName;
+    }
+  }
+  return morseAlphabet;
+};
+
+reverse = invert(alphabet);
+console.log(reverse);
 
 const generateMorse = (alphabet) => {
-  const morseChecker = userInput.map(function (letter)  {
+  const morseChecker = userInput.map(function (letter) {
     const morseTranslate = alphabet[letter];
     return morseTranslate;
-  })
-  return morseChecker.join("");
-}
+  });
+  return morseChecker.join('');
+};
 
-
-// event listner for the translate button when hit will spit back morse or english 
-translateButton.addEventListener("click", (event) => {
-  event.preventDefault();
-  userInput = input.value.split("");
-  morseDisplay.innerHTML = generateMorse(alphabet); 
-});
-
-let clicked = false;
-
-const toggleButton = () => {
-  if (!clicked) {
-    clicked = true;
-    document.querySelector("#morse__selector").innerHTML = "english to morse code";
-  } else {
-    clicked = false;
-    document.querySelector("#morse__selector").innerHTML = "morse to english";
-  }
-}
-
-const morseToEnglish = (alphabet) => {
+const generateEnglish = (reverse) => {
+  console.log('inside',reverse);
   const englishChecker = userInput.map(function (morse) {
-    const englishTranslate = alphabet[letter];
-    return englishTranslate;
-  })
+    const englishTranslate = reverse[morse]
+    return englishTranslate
+  });
+  console.log(englishChecker);
   return englishChecker.join('');
-}
+};
 
+translateButton.addEventListener('click', (event) => {
+  event.preventDefault();
 
-
-
-
-function getKeyByValue(object, value) {
-  return Object.keys(object).find(key => object[key] === value);
-}
-
-
-console.log(getKeyByValue(alphabet,".--"));
-
-
+  if (input.value.match(/[a-z0-9]/gi)) {
+    userInput = input.value.toLowerCase().split('');
+    console.log('english', userInput, typeof userInput);
+    morseDisplay.innerHTML = generateMorse(alphabet);
+  } else {
+    userInput = input.value.split('/');
+    console.log('morse', userInput, typeof userInput);
+    morseDisplay.innerHTML = generateEnglish(reverse);
+  }
+});
 
